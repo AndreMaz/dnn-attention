6ffe7c3dc916496c824aa7bf9984cf3d5b789f3f
AndreMaz <andre.maz.90@gmail.com>
2020-02-27 16:20:09 +0000

    Add tensorboard callbacks

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from dataset import generator
 # from models.seq2seq.model import createModel
 from models.luong.model import createModel
 
+from datetime import datetime
+from tensorflow import keras
+
 minYear = '1950-01-01'
 maxYear = '2050-01-01'
-
 
 def main(minYear: str, maxYear: str) -> None:
     trainEncoderInput, trainDecoderInput, trainDecoderOutput, valEncoderInput, valDecoderInput, valDecoderOutput, testDateTuples = generator.generateDataSet(
@@ -18,13 +20,18 @@ def main(minYear: str, maxYear: str) -> None:
     model = createModel(len(INPUT_VOCAB), len(OUTPUT_VOCAB), INPUT_LENGTH, OUTPUT_LENGTH)
     model.summary()
 
+
+    logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+    
     model.fit(
         x = [trainEncoderInput, trainDecoderInput],
         y = trainDecoderOutput,
-        epochs = 7,
+        epochs = 10,
         batch_size = 128,
         shuffle = True,
-        validation_data = ([valEncoderInput, valDecoderInput], valDecoderOutput )
+        validation_data = ([valEncoderInput, valDecoderInput], valDecoderOutput ),
+        # callbacks = [tensorboard_callback]
     )
 
 
